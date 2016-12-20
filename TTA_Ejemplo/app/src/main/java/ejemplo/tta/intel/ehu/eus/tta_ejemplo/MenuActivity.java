074628350d23ab52e6b55a54d70f91d7d06f5a7b
Mikel De Prado <mikeldeprado@hotmail.com>
2016-12-20 19:04:37 +0100

    Actividad login, menu y parte del test

---
+++ TTA_Ejemplo/app/src/main/java/ejemplo/tta/intel/ehu/eus/tta_ejemplo/MenuActivity.java
@@ -0,0 +1,13 @@
+package ejemplo.tta.intel.ehu.eus.tta_ejemplo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
