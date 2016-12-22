7d0f416d7ae52fd63c52d54e12542f3ad650b4d8
Mikel De Prado <mikeldeprado@hotmail.com>
2016-12-22 14:14:15 +0100

    Comenzada logica test

--- TTA_Ejemplo/app/src/main/java/ejemplo/tta/intel/ehu/eus/tta_ejemplo/TestActivity.java
+++ TTA_Ejemplo/app/src/main/java/ejemplo/tta/intel/ehu/eus/tta_ejemplo/TestActivity.java
@@ -1,13 +1,86 @@
 package ejemplo.tta.intel.ehu.eus.tta_ejemplo;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class TestActivity extends AppCompatActivity {
+import junit.framework.Test;
 
+public class TestActivity extends AppCompatActivity implements View.OnClickListener{
+    int correct=1;
+    ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+    }
+    @Override
+    public void onClick(View v)
+    {
+        findViewById(R.id.button_send_test).setVisibility(View.VISIBLE);
+    }
+
+    protected void hint()
+    {
+        String advice="Prueba consejo"
+        WebView web=new WebView(this);
+        //web.loadUrl(advise);
+        web.loadData(advice,"text/html",null);
+        web.setBackgroundColor(Color.TRANSPARENT);
+        web.setLayerType(WebView.LAYER_TYPE_SOFTWARE,null);
+        //layout.addView(web);
+
+    }
+    protected void getData()
+    {
+       /* Test test=data.getTest();
+        TextView textWording=(TextView)findViewById(R.id.test_enunciado);
+        textWording.setText(test.getWording());
+        RadioGroup group= (RadioGroup)findViewById(R.id.test_choices);
+        int i=0;
+        for(Test.Choice choice : test.getChoices())
+        {
+            RadioButton radio= new RadioButton(this);
+            radio.setText(choice.getWording());
+            radio.setOnClickListener(this);
+            group.addView(radio);
+            if(choice.isCorrect())
+            {
+                correct= i;
+            }
+            i++;
+        }*/
+
+    }
+
+    protected void send()
+    {
+       /* RadioGroup group= (RadioGroup)findViewById(R.id.test_choices);
+        int choices=group.getChildCount();
+        for(int i=0; i<choices;i++)
+            group.getChildAt(i).setEnabled(false);
+        layout.removeView(findViewById(R.id.button_send_test));
+
+        group.getChildAt(correct).setBackgroundColor(Color.GREEN);
+        if(selected != correct)
+        {
+            group.getChildAt(selected).setBackgroundColor(Color.RED);
+            Toast.makeText(getApplicationContext(),R.string.toast_fail, Toast.LENGTH_SHORT);
+            if(advise != null && !advise.isEmpty())
+            {
+                findViewById(R.id.button_hint_test).setVisibility(View.VISIBLE);
+            }
+
+        }else
+        {
+            Toast.makeText(getApplicationContext(),R.string.toast_success,Toast.LENGTH_SHORT);
+        }*/
+
     }
 }
