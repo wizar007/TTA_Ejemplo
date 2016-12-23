edf6c2e736768cf12f6946fb340e73215c2e663d
Mikel De Prado <mikeldeprado@hotmail.com>
2016-12-23 18:32:37 +0100

    Intento de Grabación de Audio

--- TTA_Ejemplo/app/src/main/java/ejemplo/tta/intel/ehu/eus/tta_ejemplo/ExerciseActivity.java
+++ TTA_Ejemplo/app/src/main/java/ejemplo/tta/intel/ehu/eus/tta_ejemplo/ExerciseActivity.java
@@ -1,13 +1,50 @@
 package ejemplo.tta.intel.ehu.eus.tta_ejemplo;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.pm.PackageManager;
+import android.widget.Toast;
 
 public class ExerciseActivity extends AppCompatActivity {
+    public final static int AUDIO_REQUEST_CODE=3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
     }
+
+    public void recordAudio(View view)
+    {
+        if(!getPackageManager().hasSystemFeature(PackageManager.FEATURE_MICROPHONE))
+        {
+            Toast.makeText(this, R.string.no_micro, Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Intent intent=new Intent(MediaStore.Audio.Media.RECORD_SOUND_ACTION);
+            if(intent.resolveActivity(getPackageManager())!=null)
+                startActivityForResult(intent, AUDIO_REQUEST_CODE);
+            else
+                Toast.makeText(this,R.string.no_app,Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode, Intent data)
+    {
+        if(resultCode != Activity.RESULT_OK)
+            return;
+        switch (requestCode)
+        {
+
+        }
+    }
+
 }
