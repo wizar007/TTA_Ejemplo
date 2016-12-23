5bddeff94c8a60a56fb183921d931af3ceb75330
Mikel De Prado <mikeldeprado@hotmail.com>
2016-12-23 17:53:02 +0100

    Añadida logica de test, comprueba errores

--- TTA_Ejemplo/app/src/main/java/ejemplo/tta/intel/ehu/eus/tta_ejemplo/TestActivity.java
+++ TTA_Ejemplo/app/src/main/java/ejemplo/tta/intel/ehu/eus/tta_ejemplo/TestActivity.java
@@ -3,8 +3,10 @@ package ejemplo.tta.intel.ehu.eus.tta_ejemplo;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.webkit.WebView;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -14,27 +16,32 @@ import junit.framework.Test;
 
 public class TestActivity extends AppCompatActivity implements View.OnClickListener{
     int correct=1;
-    ;
+    //Layout layout=findViewById(R.id.test_layout);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_test);
+        getData();
     }
     @Override
     public void onClick(View v)
     {
+
         findViewById(R.id.button_send_test).setVisibility(View.VISIBLE);
     }
 
-    protected void hint()
+    protected void hint(View view)
     {
-        String advice="Prueba consejo";
+        Layout layout=new LinearLayout();
+        String advice="<html><body><h5>Prueba consejo</h5></body></html>";
         WebView web=new WebView(this);
         //web.loadUrl(advise);
         web.loadData(advice,"text/html",null);
         web.setBackgroundColor(Color.TRANSPARENT);
         web.setLayerType(WebView.LAYER_TYPE_SOFTWARE,null);
-        //layout.addView(web);
+        layout.addView(web);
 
     }
     protected void getData()
@@ -44,15 +51,18 @@ public class TestActivity extends AppCompatActivity implements View.OnClickListe
         radio.setText("Respuesta 1");
         radio.setOnClickListener(this);
         group.addView(radio);
-        radio.setText("Respuesta 2");
-        radio.setOnClickListener(this);
-        group.addView(radio);;
-        radio.setText("Respuesta 3");
-        radio.setOnClickListener(this);
-        group.addView(radio);
-        radio.setText("Respuesta 4");
-        radio.setOnClickListener(this);
-        group.addView(radio);
+        RadioButton radio2= new RadioButton(this);
+        radio2.setText("Respuesta 2");
+        radio2.setOnClickListener(this);
+        group.addView(radio2);
+        RadioButton radio3= new RadioButton(this);
+        radio3.setText("Respuesta 3");
+        radio3.setOnClickListener(this);
+        group.addView(radio3);
+        RadioButton radio4= new RadioButton(this);
+        radio4.setText("Respuesta 4");
+        radio4.setOnClickListener(this);
+        group.addView(radio4);
        /* Test test=data.getTest();
         TextView textWording=(TextView)findViewById(R.id.test_enunciado);
         textWording.setText(test.getWording());
@@ -73,19 +83,20 @@ public class TestActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    protected void send()
+    protected void send(View view)
     {
-
-       /* RadioGroup group= (RadioGroup)findViewById(R.id.test_choices);
+        String advise="Algo diferente";
+        RadioGroup group= (RadioGroup)findViewById(R.id.test_choices);
         int choices=group.getChildCount();
         for(int i=0; i<choices;i++)
             group.getChildAt(i).setEnabled(false);
-        layout.removeView(findViewById(R.id.button_send_test));
+       // layout.removeView(findViewById(R.id.button_send_test));
 
+        int selected=group.getCheckedRadioButtonId()-1;
         group.getChildAt(correct).setBackgroundColor(Color.GREEN);
         if(selected != correct)
         {
-            group.getChildAt(selected).setBackgroundColor(Color.RED);
+            findViewById(group.getCheckedRadioButtonId()).setBackgroundColor(Color.RED);
             Toast.makeText(getApplicationContext(),R.string.toast_fail, Toast.LENGTH_SHORT);
             if(advise != null && !advise.isEmpty())
             {
@@ -95,7 +106,8 @@ public class TestActivity extends AppCompatActivity implements View.OnClickListe
         }else
         {
             Toast.makeText(getApplicationContext(),R.string.toast_success,Toast.LENGTH_SHORT);
-        }*/
+        }
+        findViewById(R.id.button_send_test).setVisibility(View.INVISIBLE);
 
     }
 }
