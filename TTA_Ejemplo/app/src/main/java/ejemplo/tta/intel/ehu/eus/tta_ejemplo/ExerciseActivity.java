26b4fc4e9b254192d979f91807766083569d6efb
Mikel De Prado <mikeldeprado@hotmail.com>
2017-01-02 17:15:03 +0100

    Captura de imagenes

--- TTA_Ejemplo/app/src/main/java/ejemplo/tta/intel/ehu/eus/tta_ejemplo/ExerciseActivity.java
+++ TTA_Ejemplo/app/src/main/java/ejemplo/tta/intel/ehu/eus/tta_ejemplo/ExerciseActivity.java
@@ -2,6 +2,8 @@ package ejemplo.tta.intel.ehu.eus.tta_ejemplo;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,8 +11,13 @@ import android.view.View;
 import android.content.pm.PackageManager;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+
 public class ExerciseActivity extends AppCompatActivity {
     public final static int AUDIO_REQUEST_CODE=3;
+    public final static int VIDEO_REQUEST_CODE=2;
+    public static final int PICTURE_REQUEST_CODE = 1;
 
 
     @Override
@@ -35,17 +42,41 @@ public class ExerciseActivity extends AppCompatActivity {
         }
 
     }
+    public void takePhoto(View view){
+        if(!getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            Toast.makeText(this,R.string.no_camera, Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if(intent.resolveActivity(getPackageManager())!= null){
+                File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+                try {
+                    File file = File.createTempFile("Tta_app",".jpg", dir);
+                    Uri photoUri = Uri.fromFile(file);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT,photoUri);
+                    startActivityForResult(intent, PICTURE_REQUEST_CODE);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            else
+            {
+                Toast.makeText(this,R.string.no_app, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
     public void recordVideo(View view)
     {
         if(!getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA))
         {
-            Toast.makeText(this, R.string.no_micro, Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, R.string.no_camera, Toast.LENGTH_SHORT).show();
         }
         else
         {
             Intent intent=new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
             if(intent.resolveActivity(getPackageManager())!=null)
-                startActivityForResult(intent, AUDIO_REQUEST_CODE);
+                startActivityForResult(intent, VIDEO_REQUEST_CODE);
             else
                 Toast.makeText(this,R.string.no_app,Toast.LENGTH_SHORT).show();
         }
@@ -57,10 +88,10 @@ public class ExerciseActivity extends AppCompatActivity {
     {
         if(resultCode != Activity.RESULT_OK)
             return;
-        switch (requestCode)
+        /*switch (requestCode)
         {
 
-        }
+        }*/
     }
 
 }
